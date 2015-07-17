da649d1fd9e1a2171cd9aad37bfa8e81c446dc1a
suyanlu <298017@qq.com>
2015-07-18 01:22:07 +0800

    增加toolbar。

--- app/src/main/java/mobi/imuse/lovesports/LoveSportsActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/LoveSportsActivity.java
@@ -2,13 +2,21 @@ package mobi.imuse.lovesports;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoveSportsActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_love_sports);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
     }
 
 }
