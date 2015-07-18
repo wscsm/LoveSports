26d4206e4628b3658b3171e03daf071cbee5a1d2
suyanlu <298017@qq.com>
2015-07-18 19:22:22 +0800

    去掉        tools:replace="android:icon"，这个属性只有当library中的icon和app中的icon重复的时候才会有效，否则会报警。

--- app/src/main/java/mobi/imuse/lovesports/PreLoginActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/PreLoginActivity.java
@@ -1,47 +1,27 @@
 package mobi.imuse.lovesports;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 
-import com.joanzapata.android.iconify.Iconify;
-
-import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class PreLoginActivity extends BaseActivity {
-
-    @Bind(R.id.btnRegister)           Button mBtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_login);
         ButterKnife.bind(this);
-        Iconify.addIcons(mBtnRegister);
         setSystemBar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_pre_login, menu);
-        return true;
+    @OnClick(R.id.btnLoginWithMobile)
+    public void onBtnLoginWithMobileClick(){
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.btnRegister)
+    public void onBtnRegisterClick(){
+        
     }
 }
