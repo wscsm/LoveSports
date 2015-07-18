a8e28604bcdccca1004d01cc812b1faef61e4b47
suyanlu <298017@qq.com>
2015-07-18 17:17:47 +0800

    增加登陆前页面.

---
+++ app/src/main/java/mobi/imuse/lovesports/BaseActivity.java
@@ -0,0 +1,27 @@
+package mobi.imuse.lovesports;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import mobi.imuse.lovesports.util.SystemBarTintManager;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void setSystemBar(){
+        //设定状态栏的颜色，当版本大于4.4时起作用
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            SystemBarTintManager tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintEnabled(true);
+            tintManager.setNavigationBarTintEnabled(true);
+            //此处可以重新指定状态栏颜色
+            tintManager.setStatusBarTintResource(R.color.primary_dark);
+            tintManager.setNavigationBarTintResource(R.color.primary_dark);
+        }
+    }
+}
--- app/src/main/java/mobi/imuse/lovesports/LoveSportsActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/LoveSportsActivity.java
@@ -1,9 +1,7 @@
 package mobi.imuse.lovesports;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
@@ -14,9 +12,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import mobi.imuse.avatar.CropperActivity;
 import mobi.imuse.lovesports.util.SLog;
-import mobi.imuse.lovesports.util.SystemBarTintManager;
 
-public class LoveSportsActivity extends AppCompatActivity {
+public class LoveSportsActivity extends BaseActivity {
     private static final String TAG = LoveSportsActivity.class.getSimpleName();
 
     @Bind(R.id.toolbar)    Toolbar mToolbar;
@@ -27,15 +24,7 @@ public class LoveSportsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_love_sports);
         ButterKnife.bind(this);
-        //设定状态栏的颜色，当版本大于4.4时起作用
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            SystemBarTintManager tintManager = new SystemBarTintManager(this);
-            tintManager.setStatusBarTintEnabled(true);
-            tintManager.setNavigationBarTintEnabled(true);
-            //此处可以重新指定状态栏颜色
-            tintManager.setStatusBarTintResource(R.color.primary_dark);
-            tintManager.setNavigationBarTintResource(R.color.primary_dark);
-        }
+        setSystemBar();
         setSupportActionBar(mToolbar);
 
 
