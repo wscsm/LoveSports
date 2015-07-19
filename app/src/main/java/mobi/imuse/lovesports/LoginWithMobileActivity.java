0d65f2d249af6b918f608a70b48aefeff280fdb0
suyanlu <298017@qq.com>
2015-07-19 18:09:53 +0800

    手机号登录界面OK。

--- app/src/main/java/mobi/imuse/lovesports/LoginWithMobileActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/LoginWithMobileActivity.java
@@ -1,14 +1,44 @@
 package mobi.imuse.lovesports;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.andreabaccega.widget.FormEditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginWithMobileActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar)    Toolbar mToolbar;
+    @Bind(R.id.editMobilePhone)    FormEditText mEditMobilePhone;
+    @Bind(R.id.editPassword)    FormEditText mEditPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_with_mobile);
+        ButterKnife.bind(this);
+
         setSystemBar();
+        setSupportActionBar(mToolbar);
     }
 
+    @OnClick(R.id.tvTitleArrowBtnLeft)
+    public void onArrowBtnLeftClick(){
+        onBackPressed();
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void onBtnLoginClick(){
+        if (!mEditMobilePhone.testValidity() || !mEditPassword.testValidity()){
+            return;
+        }
+    }
+
+    @OnClick(R.id.btnRegister)
+    public void onBtnRegisterClick(){
+
+    }
 }
