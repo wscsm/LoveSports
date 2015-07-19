44d51e5d6fa6a14241a069bc71e2b6c5633b7669
suyanlu <298017@qq.com>
2015-07-20 00:32:52 +0800

    完成80%“完善资料”页面的UI。

--- app/src/main/java/mobi/imuse/lovesports/PerfectInfoActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/PerfectInfoActivity.java
@@ -1,14 +1,35 @@
 package mobi.imuse.lovesports;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class PerfectInfoActivity extends AppCompatActivity {
+import com.andreabaccega.widget.FormEditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class PerfectInfoActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar)    Toolbar mToolbar;
+    @Bind(R.id.ivFaceImage)    ImageView mIvFaceImage;
+    @Bind(R.id.editRealName)    FormEditText mEditRealName;
+    @Bind(R.id.tvBirthday)    TextView mTvBirthday;
+    @Bind(R.id.tvCity)    TextView mTvCity;
+    @Bind(R.id.tvSportGame)    TextView mTvSportGame;
+    @Bind(R.id.editBodyLength)    FormEditText mEditBodyLength;
+    @Bind(R.id.editBodyWeight)    FormEditText mEditBodyWeight;
+    @Bind(R.id.editTeachingExperience)    FormEditText mEditTeachingExperience;
+    @Bind(R.id.tvGender)    TextView mTvGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfect_info);
+        ButterKnife.bind(this);
+        setSystemBar();
+        setSupportActionBar(mToolbar);
     }
 
 }
--- app/src/main/java/mobi/imuse/lovesports/RegisterActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/RegisterActivity.java
@@ -1,14 +1,34 @@
 package mobi.imuse.lovesports;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.andreabaccega.widget.FormEditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class RegisterActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar)    Toolbar mToolbar;
+    @Bind(R.id.editMobilePhone)    FormEditText mEditMobilePhone;
+    @Bind(R.id.editSmsAuthCode)    FormEditText mEditSmsAuthCode;
+    @Bind(R.id.editRealName)    FormEditText mEditRealName;
+    @Bind(R.id.editLoginPassword)    FormEditText mEditLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
         setSystemBar();
+        setSupportActionBar(mToolbar);
     }
 
+    @OnClick(R.id.btnNextStep)
+    public void onBtnNextStepClick() {
+        startActivity(new Intent(RegisterActivity.this, PerfectInfoActivity.class));
+    }
 }
