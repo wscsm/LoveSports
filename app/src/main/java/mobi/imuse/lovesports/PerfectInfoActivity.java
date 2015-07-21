5c8faa814e19cc7079afc2e5181bfebbb6698640
suyanlu <298017@qq.com>
2015-07-22 00:21:11 +0800

    增加生日（年月日）选择器。

--- app/src/main/java/mobi/imuse/lovesports/PerfectInfoActivity.java
+++ app/src/main/java/mobi/imuse/lovesports/PerfectInfoActivity.java
@@ -3,15 +3,20 @@ package mobi.imuse.lovesports;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.andreabaccega.widget.FormEditText;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import mobi.imuse.pickview.TimePopupWindow;
 
 public class PerfectInfoActivity extends BaseActivity {
 
@@ -43,5 +48,29 @@ public class PerfectInfoActivity extends BaseActivity {
         Intent intent = new Intent(PerfectInfoActivity.this, LoveSportsActivity.class);
         intent.putExtra("key", 6);
         startActivity(intent);
+    }
+
+    public static String getTime(Date date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
+    }
+
+    // 获取生日;
+    @OnClick(R.id.rlBirthday)
+    public void onRlBirthdayClick(){
+        TimePopupWindow pwTime;
+        //时间选择器
+        pwTime = new TimePopupWindow(PerfectInfoActivity.this, TimePopupWindow.Type.YEAR_MONTH_DAY);
+        pwTime.setRange(1970, 2100);
+        //时间选择后回调
+        pwTime.setOnTimeSelectListener(new TimePopupWindow.OnTimeSelectListener() {
+
+            @Override
+            public void onTimeSelect(Date date) {
+                mTvBirthday.setText(getTime(date));
+            }
+        });
+        //弹出时间选择器
+        pwTime.showAtLocation(mTvBirthday, Gravity.BOTTOM, 0, 0, new Date());
     }
 }
