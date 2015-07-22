9a4fa167c46aff1c2301427615cb08c0437fd6c0
suyanlu <298017@qq.com>
2015-07-22 12:11:09 +0800

    修改“完善资料”界面的显示字体。

--- app/src/main/java/mobi/imuse/lovesports/fragment/HomeFragment.java
+++ app/src/main/java/mobi/imuse/lovesports/fragment/HomeFragment.java
@@ -75,8 +75,7 @@ public class HomeFragment extends BackHandledFragment implements SwipeRefreshLay
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, rootView);
         initImageSlider();
